ed1b6715da11d278f0c72cff1e7881c3f1b30fdf
Kami1ot <petr.a.ovchinnikov@gmail.com>
2025-01-27 02:57:07 +0300

    Auth_Hello_World

---
+++ Auth_Hello_World/src/main/java/com/example/auth_hello_world/AuthHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthHelloWorldApplication.class, args);
+    }
+
+}
