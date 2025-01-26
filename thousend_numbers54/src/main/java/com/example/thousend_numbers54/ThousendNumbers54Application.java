9de7b26fe4b718858a035cdcdc98320c2e7a5f73
Kami1ot <petr.a.ovchinnikov@gmail.com>
2025-01-27 01:41:23 +0300

    thousend_numbers54

---
+++ thousend_numbers54/src/main/java/com/example/thousend_numbers54/ThousendNumbers54Application.java
@@ -0,0 +1,13 @@
+package com.example.thousend_numbers54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThousendNumbers54Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThousendNumbers54Application.class, args);
+	}
+
+}
